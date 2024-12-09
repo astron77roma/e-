2cef87e5ff3877892b0679ef72c1c93702c7d40f
astron77roma <astron77roma98@mail.ru>
2024-12-09 18:25:48 +0300

    Add files via upload

---
+++ main_2_2.py
@@ -0,0 +1,9 @@
+first = int(input("Введите Первое Число:"))
+second = int(input("Введите Второе Число:"))
+third = int(input("Введите Третье Число:"))
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == first or second == third:
+    print(2)
+else:
+    print(0)
